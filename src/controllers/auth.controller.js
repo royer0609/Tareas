9098685408dd5b4d302c2c381f5d6dd7e687fef6
royer0609 <royermontes0609@gmail.com>
2023-11-15 17:14:12 -0500

    creacion de token y encriptado hash

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,31 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcrypt';
+import { createAccesToken } from '../libs/jwt.js';
+import { token } from 'morgan';
 
 export const register = async (req, res) => {
     const {email, password, username} = req.body
 
     try {
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
         const newUser = new User({
             username,
             email,
-            password,
+            password: passwordHash,
         });
     
-        await newUser.save();
-        res.send("registrado");
+        const userSaved = await newUser.save();
+        const token = await createAccesToken({ id: userSaved._id });
+        res.cookie('token', token)
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+        });
+
+
     } catch (error) {
         console.log(error);
     }
